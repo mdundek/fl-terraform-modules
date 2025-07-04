21b34523e57d11c32ae95990e18de7ec7dad19b7
mdundek <mdundek@ea.com>
2025-07-04 12:37:31 -0400

    refactor

--- modules/aurora/update.sh
+++ modules/aurora/update.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 GIT_RELEASE=$1
-GIT_RELEASE=$(echo "$GIT_RELEASE" | tr -d ' \n\r')
 
 cd ../../bin/vpc_cleanup
 go build -ldflags="-s -w" -o ../../modules/aurora/bin/delete_sgs main.go
@@ -14,7 +13,7 @@ git add .
 git commit -m "refactor"
 git push
 
-echo $GIT_RELEASE
 
-git tag -a "$GIT_RELEASE" -m "Release $GIT_RELEASE"                                                                                                                            ✔ 
-# git push origin $GIT_RELEASE
+
+git tag -a "$1" -m "Release $1"                                                                                                                            ✔ 
+# git push origin $1
