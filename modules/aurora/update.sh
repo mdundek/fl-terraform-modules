6a77ad7b838a568b33b395694a39b47edba719d3
mdundek <mdundek@ea.com>
2025-07-04 12:40:31 -0400

    refactor

--- modules/aurora/update.sh
+++ modules/aurora/update.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <git_release_tag>"
+    exit 1
+fi
+
+set -x
+
 GIT_RELEASE=$1
 
 cd ../../bin/vpc_cleanup
 go build -ldflags="-s -w" -o ../../modules/aurora/bin/delete_sgs main.go
 cd ../..
 
-
-
-
 git add .
 git commit -m "refactor"
 git push
 
+echo "GIT_RELEASE=[$GIT_RELEASE]"
 
-
-git tag -a "$1" -m "Release $1"                                                                                                                            ✔ 
-# git push origin $1
+git tag -a "$GIT_RELEASE" -m "Release $GIT_RELEASE"                                                                                                                           ✔ 
+# git push origin $GIT_RELEASE
